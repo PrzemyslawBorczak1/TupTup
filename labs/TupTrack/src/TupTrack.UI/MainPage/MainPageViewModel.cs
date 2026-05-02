99ac9350d3a0be8b7c65ca7c9062b5ce8a5785f7
PrzemyslawBorczak1 <przemyslawborczak@gmail.com>
2026-05-02 11:47:55 +0200

    Di for MainPage

--- labs/TupTrack/src/TupTrack.UI/MainPage/MainPageViewModel.cs
+++ labs/TupTrack/src/TupTrack.UI/MainPage/MainPageViewModel.cs
@@ -3,23 +3,78 @@ using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using TupTrack.Domain;
+using TupTrack.Infrastructure;
 
 namespace TupTrack.UI.MainPage
 {
     public partial class MainPageViewModel : ObservableObject
     {
+        private AppDatabase _appDb;
+
         [ObservableProperty]
         private TupState tupState = TupState.Flat;
 
+        [ObservableProperty]
+        private string choosenRoom = "";
+
+        [ObservableProperty]
+        private SensorSpeed choosenSpeedSensor = SensorSpeed.Default;
+
+        [ObservableProperty]
+        private string choosenGroup = "";
+
+
+        [ObservableProperty]
+        private string note = "Note";
+
+
 
         [RelayCommand]
-        private void SetTupState(TupState state)
+        private void SetTupState(TupState state) =>  TupState = state;
+
+
+        public ObservableCollection<string> Rooms { get; } = new ObservableCollection<string>();
+        public ObservableCollection<SensorSpeed> SpeedSensor { get; } = new ObservableCollection<SensorSpeed>
         {
-            TupState = state;
+            SensorSpeed.Default,
+            SensorSpeed.UI,
+            SensorSpeed.Game,
+            SensorSpeed.Fastest,
+        };
+        public ObservableCollection<string> Groups { get; } = new ObservableCollection<string>();
+
+        
+        partial void OnChoosenRoomChanged(string? value)
+        {
+            if (value is null)
+            {
+                return;
+            }
+            Debug.WriteLine(choosenRoom);
         }
 
-        public ObservableCollection<string> Rooms { get; } = new ObservableCollection<string> { 
-            "Room1",
+        partial void OnChoosenSpeedSensorChanged(SensorSpeed value)
+        {
+            Debug.WriteLine(ChoosenSpeedSensor);
+        }
+
+        partial void OnChoosenGroupChanged(string value)
+        {
+            Debug.WriteLine(ChoosenGroup);
+        }
+
+
+
+        public MainPageViewModel(AppDatabase appDatabase)
+        {
+            _appDb = appDatabase;
+        }
+
+        public async Task LoadOptions()
+        {
+            var a  = new ObservableCollection<string>
+            {
+                "Room1",
             "Room2",
             "Room3",
             "Room4",
@@ -50,37 +105,7 @@ namespace TupTrack.UI.MainPage
             "Room67",
         };
 
-        [ObservableProperty]
-        private string choosenRoom = "";
-
-        partial void OnChoosenRoomChanged(string? value)
-        {
-            if (value is null)
-            {
-                return;
-            }
-            Debug.WriteLine(choosenRoom);
-        }
-
-
-        public ObservableCollection<SensorSpeed> SpeedSensor { get; } = new ObservableCollection<SensorSpeed>
-        {
-            SensorSpeed.Default,
-            SensorSpeed.UI,
-            SensorSpeed.Game,
-            SensorSpeed.Fastest,
-        };
-
-        [ObservableProperty]
-        private SensorSpeed choosenSpeedSensor = SensorSpeed.Default;
-
-        partial void OnChoosenSpeedSensorChanged(SensorSpeed value)
-        {
-            Debug.WriteLine(ChoosenSpeedSensor);
-        }
-
-
-        public ObservableCollection<string> Groups { get; } = new ObservableCollection<string>
+            var b = new ObservableCollection<string>
         {
             "Gr1",
             "Gr2",
@@ -89,18 +114,9 @@ namespace TupTrack.UI.MainPage
             "Gr5",
         };
 
-        [ObservableProperty]
-        private string choosenGroup = "";
 
-        partial void OnChoosenGroupChanged(string value)
-        {
-            Debug.WriteLine(ChoosenGroup);
         }
 
-
-
-        [ObservableProperty]
-        private string note = "Note";
 
 
 
